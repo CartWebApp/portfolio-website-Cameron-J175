5ebd19656c96f619e7e47ab20b4597900365017a
Cameron-J175 <cameron.jennings24@student.cart.org>
2025-05-07 15:48:16 -0700

    project pages done and writing pages almost done

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function myFunction() {
       }
     }
   }
+
+
+// Below creates an automatic slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+// gets rid of the previous slide/image
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+// progresses the slide/image
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
